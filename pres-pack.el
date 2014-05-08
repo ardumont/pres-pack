0b5c3f651a8af0bc70f4ec09bf4681b70aaf6137
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-08 19:09:03 +0200

    Rename init.el to pres-pack.el

--- pres-pack.el
+++ pres-pack.el
@@ -1,5 +1,15 @@
+;;; pres-pack.el --- presentation pack
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'install-packages-pack)
 (install-packs '(org
                  ox-reveal))
 
 (require 'ox-reveal)
 (setq org-reveal-root (format "%s/.prelude-packs/pres-pack/resources/reveal.js-2.6.1" (expand-file-name "~")))
+
+(provide 'pres-pack)
+;;; pres-pack.el ends here
