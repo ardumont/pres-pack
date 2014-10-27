a9a68a36221b35abeb76e31f9b6f7d01b891ee12
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-27 10:18:26 +0000

    Update install-packages-pack api call.

--- pres-pack.el
+++ pres-pack.el
@@ -5,8 +5,8 @@
 ;;; Code:
 
 (require 'install-packages-pack)
-(install-packs '(org
-                 ox-reveal))
+(install-packages-pack/install-packs '(org
+                                       ox-reveal))
 
 (require 'ox-reveal)
 (setq org-reveal-root (format "%s/.prelude-packs/pres-pack/resources/reveal.js-2.6.1" (expand-file-name "~")))
