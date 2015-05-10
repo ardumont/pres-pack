dfdb19b3c89141674ecca0010f74a27c77377878
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-05-10 17:05:42 +0200

    Use use-package to load pres-pack's deps

--- pres-pack.el
+++ pres-pack.el
@@ -4,12 +4,10 @@
 
 ;;; Code:
 
-(require 'install-packages-pack)
-(install-packages-pack/install-packs '(org
-                                       ox-reveal))
-
-(require 'ox-reveal)
-(setq org-reveal-root (format "%s/.prelude-packs/pres-pack/resources/reveal.js-2.6.1" (expand-file-name "~")))
+(use-package ox-reveal
+  :defer 4
+  :config
+  (setq org-reveal-root (format "%s/.prelude-packs/pres-pack/resources/reveal.js-2.6.1" (expand-file-name "~"))))
 
 (provide 'pres-pack)
 ;;; pres-pack.el ends here
